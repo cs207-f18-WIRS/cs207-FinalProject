28b14f3022e154f9f9f1f0985e81bef07590ed72
robbertstruyven <42975779+robbertstruyven@users.noreply.github.com>
2018-10-18 03:46:20 -0400

    Update and rename test_linear_roots.py to test_autodif.py

--- AD/test_autodif.py
+++ AD/test_autodif.py
@@ -1,13 +1,13 @@
 import pytest
-import roots
+import autodif
 
 def test_linearoots_result():
-    assert roots.linear_roots(2.0, -3.0) == 1.5
+    assert autodif.linear_roots(2.0, -3.0) == 1.5
 
 def test_linearroots_types():
     with pytest.raises(TypeError):
-        roots.linear_roots("ocean", 6.0)
+        autodif.linear_roots("ocean", 6.0)
 
 def test_linearroots_zerocoeff():
     with pytest.raises(ValueError):
-        roots.linear_roots(a=0.0)
+        autodif.linear_roots(a=0.0)
