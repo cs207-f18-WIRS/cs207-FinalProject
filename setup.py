5b088e05edab5be3e09bc54c2ddab09c08af46c5
robbertstruyven <struyven@g.harvard.edu>
2018-11-06 16:26:56 -0500

    setup

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
     
 setuptools.setup(
-    name='AD',
-    version='0.0.1',
+    name='AD_testing_packaging_CS207',
+    version='0.0.0',
     author='Shenghao Jiang, Isabelle Feldhaus, Robbert Struyven, William Wang',
     author_email=" ",
     description='Automatic Differentiation Package',
