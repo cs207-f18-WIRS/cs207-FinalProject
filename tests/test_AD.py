d0d798afc7dc549ee20a8846d3f470921df48993
isabelle <ifeld03@gmail.com>
2018-11-08 20:35:29 -0500

    updating test_AD.py

--- tests/test_AD.py
+++ tests/test_AD.py
@@ -1,51 +1,52 @@
 import pytest
-from interpreter import Lexer, Parser, Interpreter
+from AD import autodif as autodif
 
 def test_f1():
     f1 = "POW(x, POW(x, x))";
-    vd = "x:2";
-    lexer = Lexer(f1);
-    parser = Parser(lexer);
-    interpreter = Interpreter(parser);
-    dx=interpreter.differentiate(vd, dv='x');
-    assert dx==107.1104124466013909813974174582298585475873975011072538
+    vd = "x:2.0";
+    F1 = autodif.AD(f1)
+    F1.set_point(vd)
+    assert math.fabs(F1.val() - 16) < 1e-7
+    assert math.fabs(F1.diff("x") - 107.1104124466013909) < 1e-7
 
 def test_f2():
-    f1 = "EXP(3*X+X*POW(Y,2))";
-    vd ="X:1,Y:2";
-    lexer = Lexer(f1);
-    parser = Parser(lexer);
-    interpreter = Interpreter(parser);
-    dx=interpreter.differentiate(vd, dv='X');
-    dy=interpreter.differentiate(vd, dv='Y');
-    assert dx==7676.432108999210194846041668016850027095533943835292006
-    assert dy==4386.532633713834397054880953152485729768876539334452575
+    f1 = "EXP(3.0*X+X*POW(Y,2))";
+    vd ="X:1.0,Y:2.0";
+    F1 = autodif.AD(f1)
+    F1.set_point(vd)
+    assert math.fabs(F1.val() - 1096.6331584284585992) < 1e-7
+    assert math.fabs(F1.diff("X") - 7676.432108999210194) < 1e-7
+    assert math.fabs(F1.diff("Y") - 4386.532633713834397) < 1e-7
 
 def test_f3():
     f1 = "EXP(3+LOG(POW(X,2)))";
-    vd ="X:2";
-    lexer = Lexer(f1);
-    parser = Parser(lexer);
-    interpreter = Interpreter(parser);
-    dx=interpreter.differentiate(vd, dv='X');
-    assert dx==80.342147692750670963714118618326871587951631354216600577
+    vd ="X:2.0";
+    F1 = autodif.AD(f1)
+    F1.set_point(vd)
+    assert math.fabs(F1.val() - 80.342147692750670) < 1e-7
+    assert math.fabs(F1.diff("X") - 80.342147692750670) < 1e-7
 
 def test_f4():
-    f1 = "SIN(2/X)-COS(3*X)";
-    vd ="X:5";
-    lexer = Lexer(f1);
-    parser = Parser(lexer);
-    interpreter = Interpreter(parser);
-    dx=interpreter.differentiate(vd, dv='X');
-    assert dx==1.877178640951119790865338674402466846521587000496647133
+    f1 = "SIN(2.0/X)-COS(3*X)";
+    vd ="X:5.0";
+    F1 = autodif.AD(f1)
+    F1.set_point(vd)
+    assert math.fabs(F1.val() - 1.1491062551674717) < 1e-7
+    assert math.fabs(F1.diff("X") - 1.877178640951119) < 1e-7
 
 def test_f5():
-    f1 = "SIN(POW(X, 2))*EXP(COS(3*Y))";
-    vd ="X:2,Y:3";
-    lexer = Lexer(f1);
-    parser = Parser(lexer);
-    interpreter = Interpreter(parser);
-    dx=interpreter.differentiate(vd, dv='X');
-    dy=interpreter.differentiate(vd, dv='Y');
-    assert dx==10.71391946063094313319444375622570288650375264167699750
-    assert dy==-0.08789937978525762958460081495689783503136765274262355
+    f1 = "SIN(POW(X, 2))*EXP(COS(3.0*Y))";
+    vd ="X:2.0,Y:3.0";
+    F1 = autodif.AD(f1)
+    F1.set_point(vd)
+    assert math.fabs(F1.val() - -0.30428721849) < 1e-7
+    assert math.fabs(F1.diff("X") - -1.05124071609) < 1e-7
+    assert math.fabs(F1.diff("Y") - 0.37620716268) < 1e-7
+
+def test_f6():
+    f1 = "POW(2, X)";
+    vd = "X:2.0";
+    F1 = autodif.AD(f1)
+    F1.set_point(vd)
+    assert math.fabs(F1.val() - 4) < 1e-7
+    assert math.fabs(F1.diff("X") - 2.772588722239781) < 1e-7
