d7b47e4055acf2a6755762a2b1a7902a4e4929cd
William Wang <williamlw999@users.noreply.github.com>
2018-11-08 21:36:35 -0500

    tests to cover autodif

--- tests/test_AD.py
+++ tests/test_AD.py
@@ -3,16 +3,16 @@ import math
 from AD import autodif as autodif
 
 def test_f1():
-    f1 = "POW(x, POW(x, x))";
-    vd = "x:2.0";
+    f1 = "POW(x, POW(x, x))"
+    vd = "x:2.0"
     F1 = autodif.AD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 16) < 1e-7
     assert math.fabs(F1.diff("x") - 107.1104124466013909) < 1e-7
 
 def test_f2():
-    f1 = "EXP(3.0*X+X*POW(Y,2))";
-    vd ="X:1.0,Y:2.0";
+    f1 = "EXP(3.0*X+X*POW(Y,2))"
+    vd ="X:1.0,Y:2.0"
     F1 = autodif.AD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 1096.6331584284585992) < 1e-7
@@ -20,24 +20,24 @@ def test_f2():
     assert math.fabs(F1.diff("Y") - 4386.532633713834397) < 1e-7
 
 def test_f3():
-    f1 = "EXP(3+LOG(POW(X,2)))";
-    vd ="X:2.0";
+    f1 = "EXP(3+LOG(POW(X,2)))"
+    vd ="X:2.0"
     F1 = autodif.AD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 80.342147692750670) < 1e-7
     assert math.fabs(F1.diff("X") - 80.342147692750670) < 1e-7
 
 def test_f4():
-    f1 = "SIN(2.0/X)-COS(3*X)";
-    vd ="X:5.0";
+    f1 = "SIN(2.0/X)-COS(3*X)"
+    vd ="X:5.0"
     F1 = autodif.AD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 1.1491062551674717) < 1e-7
     assert math.fabs(F1.diff("X") - 1.877178640951119) < 1e-7
 
 def test_f5():
-    f1 = "SIN(POW(X, 2))*EXP(COS(3.0*Y))";
-    vd ="X:2.0,Y:3.0";
+    f1 = "SIN(POW(X, 2))*EXP(COS(3.0*Y))"
+    vd ="X:2.0,Y:3.0"
     F1 = autodif.AD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - -0.30428721849) < 1e-7
@@ -45,9 +45,35 @@ def test_f5():
     assert math.fabs(F1.diff("Y") - 0.37620716268) < 1e-7
 
 def test_f6():
-    f1 = "POW(2, X)";
-    vd = "X:2.0";
+    f1 = "POW(2, X)"
+    vd = "X:2.0"
     F1 = autodif.AD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 4) < 1e-7
     assert math.fabs(F1.diff("X") - 2.772588722239781) < 1e-7
+
+def test_noVd():
+    f1 = "x+y"
+    vd = "x:1,y:1"
+    
+    F1 = autodif.AD(f1)
+    try:
+        F1.diff_all()
+    except:
+        ret = F1.diff_all(vd)
+        for k, v in ret:
+            assert(v == 1)
+        return
+    assert(False)
+
+def test_smallVd():
+    F1 = autodif.Ad("x")
+    f2 = "x*y"
+    vd = "x:3"
+    F1.new_formula(f2)
+    try:
+        F1.val(vd)
+    except:
+        assert(F1.val("x:3,y:2")==6)
+        return
+    assert(False)
