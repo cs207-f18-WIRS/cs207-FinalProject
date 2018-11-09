39d40468975e53e4734bef21f9cbe255996bbf4b
robbertstruyven <42975779+robbertstruyven@users.noreply.github.com>
2018-11-08 19:19:43 -0500

    Update __init__.py

--- AD/__init__.py
+++ AD/__init__.py
@@ -1,2 +1,2 @@
-from AD.interpreter import Lexer, Parser, Interpreter
-print("Installation Succes!\nYou can now use the Lexer, Parser or Interpreter by typing\nAD.interpreter.Lexer(..)")
+from AD.autodif as autodif
+print("Importing Succes!")
