779e44a07ffd6f36baf2887c4f3a83c6acae6091
robbertstruyven <struyven@g.harvard.edu>
2018-12-09 13:26:48 -0500

    updating name of packaging

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
     
 setuptools.setup(
-    name='AD_testing_packaging_CS207',
-    version='0.1.5',
+    name='AD-cs207',
+    version='0.0.0',
     author='Shenghao Jiang, Isabelle Feldhaus, Robbert Struyven, William Wang',
     author_email=" ",
     description='Automatic Differentiation Package',
