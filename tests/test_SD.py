494b862ce8844dd47bd44a08f73f37ad1ae5b755
William Wang <williamlw999@users.noreply.github.com>
2018-12-10 19:46:07 -0500

    Update test_SD.py

--- tests/test_SD.py
+++ tests/test_SD.py
@@ -235,28 +235,28 @@ def test_11():
     
 def test_float():
     f1 = "5e-10"
-    vd = ""
+    vd = "x:2"
     F1 = symdif.SD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 5e-10) < 1e-7
     
 def test_float1():
     f1 = "5.11"
-    vd = ""
+    vd = "x:2"
     F1 = symdif.SD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 5.11) < 1e-7
     
 def test_float2():
     f1 = "5.111"
-    vd = ""
+    vd = "x:2"
     F1 = symdif.SD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - 5.111) < 1e-7
     
 def test_float3():
     f1 = "e*pi"
-    vd = ""
+    vd = "x:2"
     F1 = symdif.SD(f1)
     F1.set_point(vd)
     assert math.fabs(F1.val() - math.e*math.pi) < 1e-7
